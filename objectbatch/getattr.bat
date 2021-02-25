9209bfd30b4ca9f991752813bc76d7c3d25e2601
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-02-25 22:20:15 +0100

    Fix getattr

--- objectbatch/getattr.bat
+++ objectbatch/getattr.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-	if exist %~1 ( set objptr=%~1 ) else ( set objptr=!%~1! )
-	set attr=%~2
-	set rv=!%objptr%.%attr%!
+	if exist %~1 ( set "objptr=%~1" ) else ( set "objptr=!%~1!" )
+	set attr_=%~2
+	set rv=!%objptr%.%attr_%!
 endlocal & set attr=%rv%
