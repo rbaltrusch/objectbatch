8a15f171eecaf56a0b3d85c0763f0b1361036f0b
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-03-07 19:01:34 +0100

    Remove unnecessary echo calls

--- objectbatch/getptr.bat
+++ objectbatch/getptr.bat
@@ -14,9 +14,7 @@ goto :main
 			set rv=!%ptr%!a
 			set /a recursion_level=!recursion_level!-1
 			if !recursion_level! GTR 0 (
-				echo %rv% ... %rv:~0,1%
 				if %rv:~0,1% EQU $ (
-					echo !rv:~0.1! 123
 					set ptr=!rv:~0,-1!
 					goto :getval
 				)
