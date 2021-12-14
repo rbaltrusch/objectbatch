abd2b705ab1acbe99149c89954f3f5fece7a9802
Richard Baltrusch <richard@baltrusch.net>
2021-12-14 18:08:37 +0100

    Add test workflow

---
+++ tests/run_tests.bat
@@ -0,0 +1,17 @@
+@echo off
+::runs batest.bat in the batest folder. Used for a Github Actions Workflow.
+::Note that batest does not and cannot return errorlevel if a test failed (windows bug)
+::Instead, this is solved by echoing out the number of failed tests and redirecting this.
+
+set ERRORLEVEL=0
+set tempfile=__temp__
+
+dir /b ../batest
+echo.
+dir /b ../batest/batest
+
+call "../batest/batest/batest.bat" ../objectbatch > %tempfile%
+set /p ERRORLEVEL= < %tempfile%
+del %tempfile%
+echo %ERRORLEVEL% error(s) occured.
+exit /b %ERRORLEVEL%
