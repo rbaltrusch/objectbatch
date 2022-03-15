3432ce690857090b61b5dc522d38c75a44fdb2b2
Richard Baltrusch <richard@baltrusch.net>
2022-03-15 22:06:29 +0100

    Keep savefile when initially present

--- objectbatch/save.bat
+++ objectbatch/save.bat
@@ -9,9 +9,6 @@ goto :main
 
 :main 1:savefile 2:elements
 	setlocal enabledelayedexpansion
-		::remove savefile
-		if exist %~1 ( del %~1 )
-		
 		::loop through elements and redirect them to savefile
 		set counter=1
 		for %%a in ( %* ) do (
